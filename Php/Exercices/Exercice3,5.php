ee1162db7e2617d35d080ffe80b9934632570c2e
MarieSre <marieserrure@outlook.fr>
2020-09-16 14:39:11 +0200

    v9

--- Php/Exercices/Exercice3,5.php
+++ Php/Exercices/Exercice3,5.php
@@ -1,14 +1,10 @@
 <?php
 $nb1 = readline("Ecrivez un nombre :");
 $nb2 = readline("Ecrivez un nombre :");
-if (ctype_alpha($nb1)) {
-    echo "Veuillez entrer un nombre et non une lettre.";
-} else if (ctype_alpha($nb2)) {
-    echo "Veuillez entrer un nombre et non une lettre.";
-} else if ($nb1 == 0 & $nb2 == 0) {
+if ($nb1 == 0 or $nb2 == 0) {
     echo "Le produit est nul";
-} else if ($nb1 > 0 & $nb2 > 0) {
-    echo "Le produit est positif";
-} else {
+} else if ($nb1 <= -1 && $nb2 > 0 or $nb1 > 0 && $nb2 < -1) {
     echo "Le produit est négatif";
+} else if ($nb1 > 0 or $nb2 > 0 or $nb1 <= -1 or $nb2 <= -1) {
+    echo "Le produit est positif";
 } //Normallement, la machine ne devrait plus donner de réponse érronée à l'utilisateur grace à ctype_alpha
--- Php/Exercices/Exercice4,5.php
+++ Php/Exercices/Exercice4,5.php
@@ -1,9 +1,14 @@
 <?php
-$genre = readline("Êtes vous un homme ou une femme ? :");
-if ($genre = "homme") {
+$genre= strtoupper(readline("Êtes vous un homme ou une femme ? :"));
+while ($genre!= "homme" && $genre!= "femme" && $genre!= "f" && $genre!="h"){
+}
+    if ($genre = "homme" OR $genre="f") {
     $age = readline("Quel âge avez-vous :");
-}if ($age >= 20) {
-    echo "Vous êtes imposable";
-} else {
-    echo "Vous n'êtes pas imposable";
+}   if ($age >= 20) {
+        echo "Vous êtes imposable";
+} 
+    else if ($genre = "femme" OR $genre="f" && $age>=18 && $age<=35){
+        echo "Vous êtes imposable";
+}   else {
+        echo "Vous n'êtes pas imposable";
 }
