b80142821b4d906829dee261c01a131b3bb798aa
MarieSre <marieserrure@outlook.fr>
2020-09-18 10:15:14 +0200

    v14

---
+++ Php/Exercices/Exercice6,1.php
@@ -0,0 +1,9 @@
+<?php
+for ($i=0 ; $i<7 ; $i++){
+    $zero=readline("Entrer une valeur");
+       $tableau[$i]=$zero;
+    }
+    for ($i=0 ; $i<7 ; $i++){
+        echo $tableau[$i]."\t";
+    }
+  
---
+++ Php/Exercices/Exercice6,2.php
@@ -0,0 +1,10 @@
+<?php
+/*for ($i=0 ; $i<6 ; $i++){
+    $voyelle=readline("Entrer les voyelles latine :");
+       $tableau["a"]=$voyelle;
+    }
+    for ($i=0 ; $i<7 ; $i++){
+        echo $tableau[$voyelle]."\t";
+    }*/
+  $tableau=array("Pomme", "Poire", "Banane");
+    echo $tableau["Pomme"];
