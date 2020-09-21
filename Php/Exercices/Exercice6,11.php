9278766372596e41584e1536e07c47f1d7e041a7
MarieSre <marieserrure@outlook.fr>
2020-09-21 22:18:47 +0200

    v18

--- Php/Exercices/Exercice6,11.php
+++ Php/Exercices/Exercice6,11.php
@@ -1,2 +1,4 @@
 <?php
 require "FonctionsTableaux.php";
+$tab1 = creerTableauAvecTaille(4);
+$tab2 = creerTableauAvecTaille(3);
--- Php/Exercices/Exercice6,12.php
+++ Php/Exercices/Exercice6,12.php
@@ -1,5 +1,5 @@
 <?php
-
+//Cet exercice reprend le principe du 6.8 si ce n'est qu'il fait ajouté 1 à chaque valeur
 require "FonctionsTableaux.php"; //Appel des fonctions se trouvant dans ce fichier
 
 $tab1 = creerTableauTermineParZero(); // Crée un tableau qui demande une saisie utilisateur et qui s'arrête lorsqu'il saisi 0
