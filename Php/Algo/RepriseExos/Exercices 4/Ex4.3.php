e256df7c0cd395bec7f5d45e65f418bf7fce2fbe
59011-95-13 <59011-95-13@exchange.ad.afpanet>
2020-10-08 10:59:21 +0200

    AlgoV4

--- Php/Algo/RepriseExos/Exercices 4/Ex4.3.php
+++ Php/Algo/RepriseExos/Exercices 4/Ex4.3.php
@@ -7,24 +7,24 @@ $h = readline("Entrer l'heure : ");
 $m = readline("Entrer le nombre de minute : ");
 $s = readline("Entrer le nombre de seconde : ");
 
-if (($h < 24) && ($m < 59) && ($s < 59)) // On ne peut afficher 24 heures 60 minutes et 60 secondes
+if (($h < 24) && ($m <= 59) && ($s <= 59)) // On ne peut afficher 24 heures 60 minutes et 60 secondes
 {
 	$s++; // Ajoute une seconde
+
+	if ($s >= 59) 
+	{
+		$m++;
+		$s = 0;
+	}
+	if ($m == 60) 		// Si les minutes sont égal à 60
+	{
+		$h++; 			// Ajoute une heure
+		$m = 0; 		// Remet les minutes à 0
+		$s = 0; 		// Remet les secondes à 0
+	}
 }
-else if ($s == 59)
-{
-    $m++;
-    $s = 0;
-}
-/*else if (($s == 59) && ($m == 59)) // Si les minutes et les secondes sont égal à 59
-{
-	$h++; // Ajoute une heure
-	$m = 0; // Remet les minutes à 0
-	$s = 0; // Remet les secondes à 0
-}*/
 
 // Affichage
 
 echo "\nDans une minute, il sera " . $h . " heure(s) " . $m . " minute(s) et " . $s . " seconde(s)\n";
 
-// Revenir sur l'exercice pour ajouter une heure lorsque les minutes et les secondes sont égal à 59
